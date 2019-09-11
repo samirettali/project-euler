45ec67c803211f7edd04f328898bc4740666ea0b
Samir Ettali <ettali.samir@gmail.com>
2019-09-12 00:53:25 +0200

    Added solution to problem 60

---
+++ 60.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+from library import primes_sieve, is_prime, combinations
+import time
+
+
+def concat_numbers(n, m):
+    return int(str(n) + str(m))
+
+
+def find_concatenables(n, primes):
+    c = set()
+    # print(n)
+    for p in primes[primes.index(n) + 1:]:
+        if is_prime(concat_numbers(n, p)) and is_prime(concat_numbers(p, n)):
+            c.add(p)
+    return c
+
+
+def find_intersection(current_set, primes, concatenables, length):
+    if len(current_set) == length:
+        return current_set
+
+    for p in primes:
+        if p not in concatenables:
+            concatenables[p] = find_concatenables(p, primes)
+        valid = True
+        for n, m in combinations(current_set + [p], 2):
+            if (m not in concatenables or n not in concatenables[m]) and (n not in concatenables or m not in concatenables[n]):
+                valid = False
+                break
+        if valid:
+            new_set = find_intersection(current_set + [p], primes, concatenables, length)
+            if new_set:
+                return new_set
+    return None
+
+
+def main():
+    primes = primes_sieve(10000)
+    concatenables = dict()
+    intersection = None
+    while not intersection:
+        intersection = find_intersection([primes.pop(0)], primes, concatenables, 5)
+    print(sum(intersection))
+
+
+if __name__ == '__main__':
+    main()
